e907b35dcbee57ccd4bff0bb1e320de7c1b12ca6
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2021-09-07 11:04:37 +0200

    Add missing imports

--- src/test/java/eu/openanalytics/phaedra/protocolservice/repository/ProtocolRepositoryTest.java
+++ src/test/java/eu/openanalytics/phaedra/protocolservice/repository/ProtocolRepositoryTest.java
@@ -4,6 +4,7 @@ import eu.openanalytics.phaedra.protocolservice.model.Protocol;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
